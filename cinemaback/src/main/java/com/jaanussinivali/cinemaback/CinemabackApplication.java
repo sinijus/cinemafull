7a5b16ec8d03c6e6403e5b64356244d5376f948d
Jaanus <134766023+sinijus@users.noreply.github.com>
2024-03-08 21:57:36 +0200

    Initial commit

---
+++ cinemaback/src/main/java/com/jaanussinivali/cinemaback/CinemabackApplication.java
@@ -0,0 +1,13 @@
+package com.jaanussinivali.cinemaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemabackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemabackApplication.class, args);
+	}
+
+}
---
+++ cinemaback/src/test/java/com/jaanussinivali/cinemaback/CinemabackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaanussinivali.cinemaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemabackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
