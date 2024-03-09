ba4594d3b358d9282f6c9858dc9f7a1753d1a8cd
Jaanus <134766023+sinijus@users.noreply.github.com>
2024-03-09 13:11:51 +0200

    Database create ja insert sql-s

---
+++ cinemaback/init-scripts/init.sql
@@ -0,0 +1,352 @@
+CREATE SCHEMA IF NOT EXISTS cinema ;
+SELECT 'Creating schema cinema' AS status;
+GRANT ALL ON SCHEMA cinema TO postgres;
+GRANT ALL ON SCHEMA cinema TO PUBLIC;
+
+-- tables
+CREATE TABLE cinema.director
+(
+    id   serial       NOT NULL,
+    name varchar(255) NOT NULL,
+    CONSTRAINT director_pk PRIMARY KEY (id)
+);
+-- Table: restriction
+CREATE TABLE cinema.restriction
+(
+    id   serial       NOT NULL,
+    name varchar(255) NOT NULL,
+    CONSTRAINT restriction_pk PRIMARY KEY (id)
+);
+
+
+-- Table: user
+CREATE TABLE cinema."user"
+(
+    id   serial       NOT NULL,
+    name varchar(255) NOT NULL,
+    CONSTRAINT user_pk PRIMARY KEY (id)
+);
+
+-- Table: country
+CREATE TABLE cinema.country
+(
+    id   serial       NOT NULL,
+    name varchar(255) NOT NULL,
+    CONSTRAINT country_pk PRIMARY KEY (id)
+);
+
+-- Table: genre
+CREATE TABLE cinema.genre
+(
+    id   serial       NOT NULL,
+    name varchar(255) NOT NULL,
+    CONSTRAINT genre_pk PRIMARY KEY (id)
+);
+
+-- Table: hall
+CREATE TABLE cinema.hall
+(
+    id       serial       NOT NULL,
+    name     varchar(255) NOT NULL,
+    seats_no int          NOT NULL,
+    CONSTRAINT hall_pk PRIMARY KEY (id)
+);
+
+-- Table: seat
+CREATE TABLE cinema.seat
+(
+    id      serial NOT NULL,
+    hall_id int    NOT NULL,
+    row     int    NOT NULL,
+    number  int    NOT NULL,
+    CONSTRAINT seat_pk PRIMARY KEY (id)
+);
+
+
+-- Table: language
+CREATE TABLE cinema.language
+(
+    id   serial       NOT NULL,
+    name varchar(255) NOT NULL,
+    CONSTRAINT language_pk PRIMARY KEY (id)
+);
+
+-- Table: movie
+CREATE TABLE cinema.movie
+(
+    id           serial        NOT NULL,
+    title        varchar(255)  NOT NULL,
+    description  varchar(2047) NOT NULL,
+    length       int           NOT NULL,
+    release_year int           NOT NULL,
+    CONSTRAINT movie_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE cinema.movie_director
+(
+    id         serial NOT NULL,
+    movie_id   int    NOT NULL,
+    director_id int    NOT NULL,
+    CONSTRAINT movie_director_pk PRIMARY KEY (id)
+);
+
+-- Table: screening
+CREATE TABLE cinema.screening
+(
+    id       serial NOT NULL,
+    movie_id int    NOT NULL,
+    hall_id  int    NOT NULL,
+    date     date   NOT NULL,
+    time     time   NOT NULL,
+    CONSTRAINT screening_pk PRIMARY KEY (id)
+);
+
+-- Table: movie_country
+CREATE TABLE cinema.movie_country
+(
+    id         serial NOT NULL,
+    movie_id   int    NOT NULL,
+    country_id int    NOT NULL,
+    CONSTRAINT movie_country_pk PRIMARY KEY (id)
+);
+
+-- Table: movie_genre
+CREATE TABLE cinema.movie_genre
+(
+    id       serial NOT NULL,
+    movie_id int    NOT NULL,
+    genre_id int    NOT NULL,
+    CONSTRAINT movie_genre_pk PRIMARY KEY (id)
+);
+
+-- Table: movie_language
+CREATE TABLE cinema.movie_language
+(
+    id          serial NOT NULL,
+    movie_id    int    NULL,
+    language_id int    NOT NULL,
+    CONSTRAINT movie_language_pk PRIMARY KEY (id)
+);
+
+-- Table: movie_restriction
+CREATE TABLE cinema.movie_restriction
+(
+    id             serial NOT NULL,
+    restriction_id int    NOT NULL,
+    movie_id       int    NULL,
+    CONSTRAINT movie_restriction_pk PRIMARY KEY (id)
+);
+
+-- Table: movie_subtitles
+CREATE TABLE cinema.movie_subtitles
+(
+    id          serial NOT NULL,
+    movie_id    int    NOT NULL,
+    language_id int    NOT NULL,
+    CONSTRAINT movie_subtitles_pk PRIMARY KEY (id)
+);
+
+-- Table: reservation
+CREATE TABLE cinema.reservation
+(
+    id           serial  NOT NULL,
+    screening_id int     NOT NULL,
+    user_id      int     NOT NULL,
+    paid         boolean NOT NULL,
+    active       boolean NOT NULL,
+    CONSTRAINT reservation_pk PRIMARY KEY (id, paid)
+);
+
+-- Table: reserved_seat
+CREATE TABLE cinema.reserved_seat
+(
+    id               serial  NOT NULL,
+    screening_id     int     NOT NULL,
+    seat_id          int     NOT NULL,
+    reservation_id   int     NOT NULL,
+    reservation_paid boolean NOT NULL,
+    CONSTRAINT reserved_seat_pk PRIMARY KEY (id)
+);
+
+
+-- foreign keys
+-- Reference: Copy_of_movie_country_language (table: movie_language)
+ALTER TABLE cinema.movie_language
+    ADD CONSTRAINT Copy_of_movie_country_language
+        FOREIGN KEY (language_id)
+            REFERENCES language (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: Copy_of_movie_country_language (table: movie_subtitles)
+ALTER TABLE cinema.movie_subtitles
+    ADD CONSTRAINT Copy_of_movie_country_language
+        FOREIGN KEY (language_id)
+            REFERENCES language (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: Copy_of_movie_country_movie (table: movie_language)
+ALTER TABLE cinema.movie_language
+    ADD CONSTRAINT Copy_of_movie_country_movie
+        FOREIGN KEY (movie_id)
+            REFERENCES movie (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: Copy_of_movie_country_movie (table: movie_subtitles)
+ALTER TABLE cinema.movie_subtitles
+    ADD CONSTRAINT Copy_of_movie_country_movie
+        FOREIGN KEY (movie_id)
+            REFERENCES movie (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: Table_3_country (table: movie_country)
+ALTER TABLE cinema.movie_country
+    ADD CONSTRAINT Table_3_country
+        FOREIGN KEY (country_id)
+            REFERENCES country (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: Table_3_movie (table: movie_country)
+ALTER TABLE cinema.movie_country
+    ADD CONSTRAINT Table_3_movie
+        FOREIGN KEY (movie_id)
+            REFERENCES movie (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+ALTER TABLE cinema.movie_director
+    ADD CONSTRAINT Table_3_country
+        FOREIGN KEY (director_id)
+            REFERENCES director (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+ALTER TABLE cinema.movie_director
+    ADD CONSTRAINT Table_3_movie
+        FOREIGN KEY (movie_id)
+            REFERENCES movie (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: movie_genre_genre (table: movie_genre)
+ALTER TABLE cinema.movie_genre
+    ADD CONSTRAINT movie_genre_genre
+        FOREIGN KEY (genre_id)
+            REFERENCES genre (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: movie_genre_movie (table: movie_genre)
+ALTER TABLE cinema.movie_genre
+    ADD CONSTRAINT movie_genre_movie
+        FOREIGN KEY (movie_id)
+            REFERENCES movie (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: movie_restriction_movie (table: movie_restriction)
+ALTER TABLE cinema.movie_restriction
+    ADD CONSTRAINT movie_restriction_movie
+        FOREIGN KEY (movie_id)
+            REFERENCES movie (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: movie_restriction_restriction (table: movie_restriction)
+ALTER TABLE cinema.movie_restriction
+    ADD CONSTRAINT movie_restriction_restriction
+        FOREIGN KEY (restriction_id)
+            REFERENCES restriction (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: reservation_screening (table: reservation)
+ALTER TABLE cinema.reservation
+    ADD CONSTRAINT reservation_screening
+        FOREIGN KEY (screening_id)
+            REFERENCES screening (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: reservation_user (table: reservation)
+ALTER TABLE cinema.reservation
+    ADD CONSTRAINT reservation_user
+        FOREIGN KEY (user_id)
+            REFERENCES "user" (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: reserved_seat_reservation (table: reserved_seat)
+ALTER TABLE cinema.reserved_seat
+    ADD CONSTRAINT reserved_seat_reservation
+        FOREIGN KEY (reservation_id, reservation_paid)
+            REFERENCES reservation (id, paid)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: reserved_seat_screening (table: reserved_seat)
+ALTER TABLE cinema.reserved_seat
+    ADD CONSTRAINT reserved_seat_screening
+        FOREIGN KEY (screening_id)
+            REFERENCES screening (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: reserved_seat_seat (table: reserved_seat)
+ALTER TABLE cinema.reserved_seat
+    ADD CONSTRAINT reserved_seat_seat
+        FOREIGN KEY (seat_id)
+            REFERENCES seat (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: screening_hall (table: screening)
+ALTER TABLE cinema.screening
+    ADD CONSTRAINT screening_hall
+        FOREIGN KEY (hall_id)
+            REFERENCES hall (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: screening_movie (table: screening)
+ALTER TABLE cinema.screening
+    ADD CONSTRAINT screening_movie
+        FOREIGN KEY (movie_id)
+            REFERENCES movie (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: seat_hall (table: seat)
+ALTER TABLE cinema.seat
+    ADD CONSTRAINT seat_hall
+        FOREIGN KEY (hall_id)
+            REFERENCES hall (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- End of file.
+
---
+++ cinemaback/init-scripts/insert.sql
@@ -0,0 +1,130 @@
+INSERT INTO cinema.country (id, name)
+VALUES (default, 'Prantsusmaa');
+INSERT INTO cinema.country (id, name)
+VALUES (default, 'Saksamaa');
+INSERT INTO cinema.country (id, name)
+VALUES (default, 'Rootsi');
+INSERT INTO cinema.country (id, name)
+VALUES (default, 'Eesti');
+INSERT INTO cinema.country (id, name)
+VALUES (default, 'Taani');
+INSERT INTO cinema.country (id, name)
+VALUES (default, 'Island');
+INSERT INTO cinema.country (id, name)
+VALUES (default, 'USA');
+INSERT INTO cinema.country (id, name)
+VALUES (default, 'Inglismaa');
+INSERT INTO cinema.country (id, name)
+VALUES (default, 'Soome');
+
+INSERT INTO cinema.language (id, name)
+VALUES (default, 'prantsuse');
+INSERT INTO cinema.language (id, name)
+VALUES (default, 'saksa');
+INSERT INTO cinema.language (id, name)
+VALUES (default, 'rootsi');
+INSERT INTO cinema.language (id, name)
+VALUES (default, 'eesti');
+INSERT INTO cinema.language (id, name)
+VALUES (default, 'taani');
+INSERT INTO cinema.language (id, name)
+VALUES (default, 'islandi');
+INSERT INTO cinema.language (id, name)
+VALUES (default, 'inglise');
+INSERT INTO cinema.language (id, name)
+VALUES (default, 'vene');
+
+INSERT INTO cinema.genre (id, name)
+VALUES (default, 'Tegevus');
+INSERT INTO cinema.genre (id, name)
+VALUES (default, 'Komöödia');
+INSERT INTO cinema.genre (id, name)
+VALUES (default, 'Romantika');
+INSERT INTO cinema.genre (id, name)
+VALUES (default, 'Ajalooline');
+INSERT INTO cinema.genre (id, name)
+VALUES (default, 'Draama');
+INSERT INTO cinema.genre (id, name)
+VALUES (default, 'Muusikaline');
+INSERT INTO cinema.genre (id, name)
+VALUES (default, 'Ulme');
+INSERT INTO cinema.genre (id, name)
+VALUES (default, 'Psühholoogiline');
+INSERT INTO cinema.genre (id, name)
+VALUES (default, 'Õudus');
+INSERT INTO cinema.genre (id, name)
+VALUES (default, 'Erootiline');
+INSERT INTO cinema.genre (id, name)
+VALUES (default, 'Lastefilm');
+INSERT INTO cinema.genre (id, name)
+VALUES (default, 'Animafilm');
+
+INSERT INTO cinema.restriction (id, name)
+VALUES (default, 'Perefilm');
+INSERT INTO cinema.restriction (id, name)
+VALUES (default, 'Alla 12 a. keelatud');
+INSERT INTO cinema.restriction (id, name)
+VALUES (default, 'Alla 14 a. keelatud');
+INSERT INTO cinema.restriction (id, name)
+VALUES (default, 'Alla 16 a. keelatud');
+
+INSERT INTO cinema.user (id, name)
+VALUES (default, 'testkasutaja');
+
+INSERT INTO cinema.hall (id, name, seats_no)
+VALUES (default, 'Peasaal', 120);
+
+INSERT INTO cinema.director (id, name)
+VALUES (default, 'David Lynch');
+INSERT INTO cinema.director (id, name)
+VALUES (default, 'Werner Herzog');
+INSERT INTO cinema.director (id, name)
+VALUES (default, 'Sergio Leone');
+INSERT INTO cinema.director (id, name)
+VALUES (default, 'Nikolaj Arcel');
+INSERT INTO cinema.director (id, name)
+VALUES (default, 'John Carney');
+INSERT INTO cinema.director (id, name)
+VALUES (default, 'Margus Paju');
+INSERT INTO cinema.director (id, name)
+VALUES (default, 'Anna Hints');
+
+
+INSERT INTO cinema.movie (id, title, description, length, release_year)
+VALUES (default, 'Blue Velvet',
+        'David Lynchi hallutsinogeenne, sensuaalne müsteerium. Pealtnäha tuttavlikus väikeses Ameerika linnas leiab noor koolipoiss Jeffrey Beaumont (Kyle Maclachlan) inimkõrva. Asja uurima hakates avastab noormees linna põrandaaluse pahupoole, kus lokkab prostitutsioon, vägivald ja perverssus. Suutmata oma uudishimu taltsutada, mässib Jeff end kauni lauljanna Dorothy Vallensi (Isabella Rossellini) ja psühhopaat Frank Boothi (Dennis Hopper) vahelisse konflikti.
+‘Blue Velvet’ teostub binaarses maagilise realismi maailmas, kus üleloomulikud nähtused sulanduvad kokku loomuliku, meile harjumuspärase maailmaga. Lynchilikult luupainajalik ja psühhoanalüütiline vis-à-vis moment Varju arhetüübiga.',
+            120, 1986);
+INSERT INTO cinema.movie (id, title, description, length, release_year)
+VALUES (default, 'Aguirre, Jumala viha',
+        'Korduvalt maailma parimate filmide hulka valitud „Aguirre, Jumala viha“ on üks Herzogi kuulsaimaid töid. See koondab endasse tema teostes esinevad korduvad teemad: looduse üleolek inimesest, inimeste julmus ja võimuiha, äärmuslikes oludes ellujäämine ja suured unistused.
+Juba filmi alguskaader, kus mikroskoopilisena näiv inimkolonn ronib mööda tohutu mäe nõlva, seab looduse ja inimese suhte õigesse mastaapi. On 1560. aasta ja Hispaania konkistadoorid otsivad Lõuna-Ameerika džunglis muinasjutulist kullalinna El Doradot. Džungli julmus põhjustab rändajates mässu ning nende juhiks kerkib hulljulge ohvitser Don Lope de Aguirre. Seltskond jätkab reisi piki Amazonast, mille kallastel varitsevad verejanulised indiaanlased. Parvetajaid painavad kuumus, niiskus ja hirm tihenevad kollektiivseks hulluseks ja lõpuks maksab loodus kätte.',
+            93, 1972);
+INSERT INTO cinema.movie (id, title, description, length, release_year)
+VALUES (default, 'Hea, paha ja inetu',
+        'Edela-USAs Kodusõja ajal on salapärane tundmatu Joe (Clint Eastwood) ja Mehhiko bandiit Tuco (Eli Wallach) sõlminud kummalise kokkuleppe – Joe teenib Tuco püüdmise eest lunarahasid ning päästab ta seejärel poomisest. Üsna pea kaovad asjad aga kontrolli alt ning mehed leiavad end võidujooksus kolmandaga, et kõrbest leida soldati maha maetud $20,000.
+Ennio Morricone suurepärase originaalmuusikaga legendaarne spagetivestern.',
+            178, 1986);
+INSERT INTO cinema.movie (id, title, description, length, release_year)
+VALUES (default, 'Tõotatud maa',
+        'Aastal 1755 asub vaesunud kapten Ludvig Kahlen vallutama karmi ja asustamata Taani nõmme-ala näiliselt võimatul eesmärgil - ehitada koloonia kuninga nimel. Vastutasuks saab ta endale väga soovitud kuningliku nime. Kuid selle piirkonna ainuvalitseja, halastamatu Frederik de Schinkel, usub, et see maa kuulub talle. Kui de Schinkel saab teada, et teenija Ann Barbara ja tema teenistujast abikaasa on põgenenud varjupaika Kahleni juurde, vannub privileegidele pretendeeriv ja pahatahtlik valitseja kättemaksu ning teeb kõik endast oleneva, et kapten sealt minema ajada. Kahlen ei lase end hirmutada ja võtab vastu ebavõrdse võitluse - riskides mitte ainult oma eluga, vaid ka ümberkaudsete võõraste peredega.',
+            120, 2023);
+INSERT INTO cinema.movie (id, title, description, length, release_year)
+VALUES (default, 'Once',
+        'Dublinis elatist teeniv tänavamuusik (Glen Hansard) tutvub ühel päeval võluva tšehhi tüdrukuga (Markéta Irglová) ning nad veedavad koos unustamatu nädala. Noored kirjutavad koos lugusid, teevad proove ja salvestavad laulud, milles on kokku võetud nende kahe unikaalne armastuslugu.
+Film on võitnud erinevaid auhindu, sealhulgas parima filmilaulu Oscari („Falling Slowly“) ning Sundance ja Dublini filmifestivali publikuauhinna. ',
+            85, 2007);
+INSERT INTO cinema.movie (id, title, description, length, release_year)
+VALUES (default, 'Supilinna Salaselts',
+        'Mari, Sadu, Olav ja Anton on neli Supilinna last, kelle rõõmus argipäev pööratakse pea peale, kui nende kodulinna tabab salapärane mürgitus, mis pooled linna täiskasvanud lasteks muudab. Lastel on lahenduse leidmiseks aega vaid 48 tundi. Algab peadpööritav seiklus, mis viib lapsed kõige ootamatumatesse paikadesse nii maa peal kui ka maa all. Praeguse kodulinna kõrval õpivad nad tundma ühte teist, aastatetagust Tartut, teejuhiks Mari vanaisa käest saadud märkmik salapäraste vihjetega.',
+            120, 2015);
+INSERT INTO cinema.movie (id, title, description, length, release_year)
+VALUES (default, 'Igaüks iseenda eest ja Jumal kõigi vastu',
+        'Saksa väikelinna väljakult leitakse räbalates noormees. Tal on peos kiri, milles palutakse nooruki eest hoolitseda. Poiss ei räägi sõnagi ja oskab vaevu püsti seista. Linlased võtavad ta oma hoole alla ning kui noormees lõpuks õpib rääkima, jutustab ta, et on kogu elu olnud keldrisse suletud. Kaspar Hauseri lugu on huvitanud inimesi juba ligi kakssada aastat. Milline koletis peidab last maailma eest ja kas on võimalik kasvada inimeseks ilma inimlike kontaktideta?
+Werner Herzogi filmi aluseks on 1828. aastast pärit originaaldokumendid ja kirjad. See on poeetiline ja kurvameelne lugu inimliku julmuse tagajärgedest. Kurjus peidab end nii pimedas keldris kui ka ahnete inimeste hulgas, kes kasutavad Hauserit ära – kord tsirkuselooma, kord kõrgema seltskonna kurioosumina. Kaspar Hauser ei tunne teiste inimeste seltskonnast rõõmu, sest usaldus on talle on võõras. Ta loob endale oma filosoofilise mõtteviisi ja üritab sellega maailma määratleda, kuid harukordse mineviku tõttu mõistetakse teda üha valesti. Tema lõpp on traagiline nagu teisitimõtlejatel ikka. „Inimesed on mulle hundid,“ nendib Kaspar Hauser filmis.',
+            110, 1974);
+INSERT INTO cinema.movie (id, title, description, length, release_year)
+VALUES (default, 'Savvusanna sõsarad',
+        'Esimese Eesti dokumentaalfilmina maineka Sundance´i filmifestivali võistlusprogrammis esilinastunud „Savvusanna sõsarad“ on kaasaegne portreefilm naistest, kes suitsusauna kaitsvas pimeduses kokku tulevad, oma sügavamaid saladusi jagavad ning kehasse kogunenud häbi ja valu maha pesevad.',
+            89, 2023);
+
