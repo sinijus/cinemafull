3be17a6d101931199892974d39c854c038062b94
Jaanus <134766023+sinijus@users.noreply.github.com>
2024-03-12 11:22:40 +0200

    setting project configurations, project is running in dev

--- cinemafront/vite.config.js
+++ cinemafront/vite.config.js
@@ -3,5 +3,30 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port:8081,
+    proxy: {
+      '/api': {
+        //for development use
+        target: 'http://localhost:8080',
+        //for Docker use
+        // target: 'http://proovjsback:8080',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  preview: {
+    host: true,
+    port: 8081
+  },
   plugins: [vue()],
+  resolve: {
+    // alias: {
+    //   '@': fileURLToPath(new URL('./src', import.meta.url))
+    // }
+  }
 })
