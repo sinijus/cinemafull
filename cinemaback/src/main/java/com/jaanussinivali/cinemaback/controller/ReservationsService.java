02f48e15c26c7f6292d38a1ed2a7382336020a9c
Jaanus <134766023+sinijus@users.noreply.github.com>
2024-03-14 12:16:44 +0200

    Added Exception handling, GET(or make) Reservation COMPLETED fix

--- cinemaback/src/main/java/com/jaanussinivali/cinemaback/controller/ReservationsService.java
+++ cinemaback/src/main/java/com/jaanussinivali/cinemaback/controller/ReservationsService.java
@@ -34,22 +34,31 @@ public class ReservationsService {
 
     public ReservationResponse findOrCreateScreeningReservation(Integer screeningId, Integer userId) {
         boolean reservationExists = reservationService.activeReservationWithUserIdAndScreeningIdExists(userId, screeningId);
+        Reservation reservation = getExistingOrCreatereNewReservation(screeningId, userId, reservationExists);
+        return reservationMapper.toReservationResponse(reservation);
+    }
+
+    private Reservation getExistingOrCreatereNewReservation(Integer screeningId, Integer userId, boolean reservationExists) {
         Reservation reservation;
         if (reservationExists) {
             reservation = reservationService.findReservationByUserIdAndScreeningId(userId, screeningId);
         } else {
             reservation = new Reservation();
-            CinemaUser user = cinemaUserService.findUser(userId);
-            Screening screening = screeningService.findScreening(screeningId);
-            Hall hall = hallService.findHall(screening.getHall().getId());
-            Movie movie = movieService.findMovie(screening.getMovie().getId());
-            screening.setMovie(movie);
-            screening.setHall(hall);
-            reservation.setScreening(screening);
-            reservation.setCinemaUser(user);
-            reservation.setActive(true);
+            getAndSetReservationFields(screeningId, userId, reservation);
             reservationService.saveNewReservation(reservation);
         }
-        return reservationMapper.toReservationResponse(reservation);
+        return reservation;
+    }
+
+    private void getAndSetReservationFields(Integer screeningId, Integer userId, Reservation reservation) {
+        CinemaUser user = cinemaUserService.findUser(userId);
+        Screening screening = screeningService.findScreening(screeningId);
+        Hall hall = hallService.findHall(screening.getHall().getId());
+        Movie movie = movieService.findMovie(screening.getMovie().getId());
+        screening.setMovie(movie);
+        screening.setHall(hall);
+        reservation.setScreening(screening);
+        reservation.setCinemaUser(user);
+        reservation.setActive(true);
     }
 }
