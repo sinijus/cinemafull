153f573ad30980176af9cbd91278f9c3613bcb50
Jaanus <134766023+sinijus@users.noreply.github.com>
2024-03-17 16:25:36 +0200

    Seat selection frame floor plan set, offered seat WIP

--- cinemafront/src/components/SeatReservationFrame.vue
+++ cinemafront/src/components/SeatReservationFrame.vue
@@ -1,16 +1,42 @@
 <template>
-  <div class="d-flex flex-column align-center pa-6">
-    <v-row v-for="(row, rowIndex) in reservationHallResponse.hall" :key="rowIndex">
-      <v-col v-for="(seat, seatIndex) in row" :key="seatIndex">
-        <v-btn-toggle v-model="seat.occupied" @change="toggleSeat(rowIndex, seatIndex)">
-          <v-btn :disabled="seat.occupied">
-            <span>Seat {{ rowIndex + 1 }} - {{ seatIndex + 1 }}</span>
-            <v-icon>mdi-chair</v-icon>
-          </v-btn>
-        </v-btn-toggle>
-      </v-col>
-    </v-row>
-  </div>
+  <v-card>
+    <div class="d-flex flex-column align-center">
+      <v-row >
+        <v-col>
+          <v-btn-toggle disabled variant="text" density="compact" style="width: 2dvi">
+            <v-btn style="font-size: smaller">
+            </v-btn>
+          </v-btn-toggle>
+        </v-col>
+        <v-col v-for="(n,index) in  reservationHallResponse.seatObjectHall[0]" :key="index">
+          <v-btn-toggle disabled variant="text" density="compact" style="width: 2dvi">
+            <v-btn style="font-size: smaller">
+              <span>{{ index }} </span>
+            </v-btn>
+          </v-btn-toggle>
+        </v-col>
+      </v-row>
+      <v-row v-for="(row, rowIndex) in reservationHallResponse.seatObjectHall" :key="rowIndex">
+        <v-col>
+          <v-btn-toggle disabled variant="text" density="compact" style="width: 2dvi">
+            <v-btn style="font-size: smaller">
+            </v-btn>
+          </v-btn-toggle>
+        </v-col>
+        <v-col v-for="(seat, seatIndex) in row" :key="seatIndex">
+          <v-btn-toggle disabled v-model="seat.available" @change="toggleSeat(rowIndex, seatIndex)"
+                        variant="tonal" density="compact" style="width: 2dvi"
+                        :class="{ 'occupied-color': !seat.available, 'selected-color': seat.selected, 'available-color': seat.available }"
+          >
+            <v-btn style="font-size: smaller">
+              <span> {{ seat.id }}</span>
+              <!--            <v-icon>mdi-chair</v-icon>-->
+            </v-btn>
+          </v-btn-toggle>
+        </v-col>
+      </v-row>
+    </div>
+  </v-card>
 </template>
 
 <script>
@@ -21,7 +47,7 @@ export default {
       seatIds: [
         Number
       ],
-      hall: [
+      seatObjectHall: [
         [
           {
             id: Number,
@@ -32,14 +58,25 @@ export default {
     }
   },
   data() {
-    return {
-      toggle: [],
-    }
+    return {}
   },
   methods: {
     toggleSeat(rowIndex, seatIndex) {
-      this.reservationHallResponse.hall[rowIndex][seatIndex].occupied = !this.reservationHallResponse.hall[rowIndex][seatIndex].occupied;
+      this.reservationHallResponse.hall[rowIndex][seatIndex].available = !this.reservationHallResponse.hall[rowIndex][seatIndex].available;
     }
   }
 }
 </script>
+<style scoped>
+.occupied-color {
+  background-color: #B71C1C;
+}
+
+.selected-color {
+  background-color: #1E88E5; /* Set the background color for selected seats */
+}
+
+.available-color {
+  background-color: #388E3C; /* Set the background color for available seats */
+}
+</style>
