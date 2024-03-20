0f06c900bcc7a866bbc7c086c9002a6b79646f5d
Jaanus <134766023+sinijus@users.noreply.github.com>
2024-03-20 15:56:36 +0200

    AppBar button for adding random reservations to screenings added

--- cinemafront/src/components/AppBar.vue
+++ cinemafront/src/components/AppBar.vue
@@ -1,5 +1,21 @@
 <template>
   <v-app-bar color="#C62828" height="120" app>
+
+    <v-menu>
+      <template v-slot:activator="{ props }">
+        <v-btn icon="mdi-dots-vertical" v-bind="props"></v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          @click="addReservationsToAllScreenings"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
     <div class="flex-grow-1"
          style="font-size: 8rem; font-weight: bold; font-style: oblique; font-family: Calibri; color: #111111">
       <span> KINO </span>
@@ -25,6 +41,19 @@
 
 <script>
 export default {
-  name: "AppBar"
+  name: "AppBar",
+  data() {
+    return {
+      items: [
+        {title: 'Lisa kõikidele seanssidele suvaline arv broneeringuid'},
+      ],
+    }
+  },
+  methods:{
+    addReservationsToAllScreenings() {
+
+
+    },
+  }
 }
 </script>
