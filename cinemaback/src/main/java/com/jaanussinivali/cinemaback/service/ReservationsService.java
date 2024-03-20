e19438f9369cd38630e2cc73777fbb3a2fdb2bd2
Jaanus <134766023+sinijus@users.noreply.github.com>
2024-03-20 14:45:26 +0200

    SeatSelectionGenerator giving already reserved id-s fix maybe WIP

--- cinemaback/src/main/java/com/jaanussinivali/cinemaback/service/ReservationsService.java
+++ cinemaback/src/main/java/com/jaanussinivali/cinemaback/service/ReservationsService.java
@@ -73,16 +73,25 @@ public class ReservationsService {
         Screening screening = screeningService.findScreening(screeningId);
         Integer hallId = screening.getHall().getId();
         List<Seat> seats = seatService.findSeatsByHallId(hallId);
-        List<ReservedSeat> reservedSeats = seatService.findReservedSeatsByScreeningIdAndReservationId(screeningId, reservationId);
+        List<ReservedSeat> reservedSeats = seatService.findReservedSeatsByScreeningId(screeningId);
         validateNumberOfSeatsRequestedIsAvailable(numberOfSeatsRequest, seats, reservedSeats);
 
         Seat lastSeat = seats.getLast();
         Integer numberOfRows = lastSeat.getRow();
         Integer numberOfSeatsInARow = lastSeat.getNumber();
 
+        List<List<SeatResponse>> seatObjectHall = setSeatObjectHall(numberOfRows, numberOfSeatsInARow, reservedSeats);
+
+        List<Integer> offeredSeatsForReservation =
+                SeatSelectionGenerator.offerSeatIndexes(numberOfSeatsRequest, seatObjectHall, numberOfRows, numberOfSeatsInARow);
+
+        setAndSaveOfferedReservedSeats(reservationId, offeredSeatsForReservation, seats, screening);
+        return setSeatReservationResponse(seatObjectHall, offeredSeatsForReservation);
+    }
+
+    private List<List<SeatResponse>> setSeatObjectHall(Integer numberOfRows, Integer numberOfSeatsInARow, List<ReservedSeat> reservedSeats) {
         List<List<SeatResponse>> seatObjectHall = new ArrayList<>();
 
-        //set seatObjectHall
         int seatIndex = 0;
         for (int i = 0; i < numberOfRows; i++) {
             List<SeatResponse> row = new ArrayList<>();
@@ -101,39 +110,35 @@ public class ReservationsService {
             }
             seatObjectHall.add(row);
         }
-
-        List<Integer> offeredSeatsForReservation =
-                SeatSelectionGenerator.offerSeatIndexes(numberOfSeatsRequest, seatObjectHall, numberOfRows, numberOfSeatsInARow);
-
-        setAndSaveOfferedReservedSeats(reservationId, offeredSeatsForReservation, seats, screening);
-        return setSeatReservationResponse(seatObjectHall, offeredSeatsForReservation);
+        return seatObjectHall;
     }
 
     private void setAndSaveOfferedReservedSeats(Integer reservationId, List<Integer> offeredSeatsForReservation, List<Seat> seats, Screening screening) {
         for (Integer offeredSeat : offeredSeatsForReservation) {
             ReservedSeat reservedSeat = new ReservedSeat();
             Reservation reservation = reservationService.findReservation(reservationId);
-            reservedSeat.setSeat(seats.get(offeredSeat));
+            Seat seat = seats.get(offeredSeat);
+            reservedSeat.setSeat(seat);
             reservedSeat.setScreening(screening);
             reservedSeat.setReservation(reservation);
             seatService.saveReservedSeat(reservedSeat);
         }
     }
 
-    private static SeatReservationResponse setSeatReservationResponse(List<List<SeatResponse>> seatObjectHall, List<Integer> offeredSeatsForReservation) {
+    private SeatReservationResponse setSeatReservationResponse(List<List<SeatResponse>> seatObjectHall, List<Integer> offeredSeatsForReservation) {
         SeatReservationResponse response = new SeatReservationResponse();
         response.setSeatObjectHall(seatObjectHall);
         response.setSeatIds(offeredSeatsForReservation);
         return response;
     }
 
-    private static void validateNumberOfSeatsRequestedIsAvailable(Integer numberOfSeatsRequest, List<Seat> seats, List<ReservedSeat> reservedSeats) {
+    private void validateNumberOfSeatsRequestedIsAvailable(Integer numberOfSeatsRequest, List<Seat> seats, List<ReservedSeat> reservedSeats) {
         if (isThereNotEnoughSeatsAvailable(numberOfSeatsRequest, seats, reservedSeats)) {
             throw new BusinessException(Error.NOT_ENOUGH_AVAILABLE_SEATS.getMessage(), Error.NOT_ENOUGH_AVAILABLE_SEATS.getErrorCode());
         }
     }
 
-    private static boolean isThereNotEnoughSeatsAvailable(Integer numberOfSeatsRequest, List<Seat> seats, List<ReservedSeat> reservedSeats) {
+    private boolean isThereNotEnoughSeatsAvailable(Integer numberOfSeatsRequest, List<Seat> seats, List<ReservedSeat> reservedSeats) {
         return seats.size() - reservedSeats.size() < numberOfSeatsRequest;
     }
 
