2b292b8cb7ae37b5f0d479f7407155bafd483dea
Jaanus <134766023+sinijus@users.noreply.github.com>
2024-03-21 11:20:23 +0200

    Tiding backend

--- cinemaback/src/main/java/com/jaanussinivali/cinemaback/controller/MovieController.java
+++ cinemaback/src/main/java/com/jaanussinivali/cinemaback/controller/MovieController.java
@@ -4,9 +4,7 @@ import com.jaanussinivali.cinemaback.dto.*;
 import com.jaanussinivali.cinemaback.service.MoviesService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.annotation.Resource;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
